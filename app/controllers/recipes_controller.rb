0bf7b8fab85d4b473091d1afd5bf9516e9b94510
Manuel Omar Pineyro & Andrew Hallberg <student@epicodus.com>
2014-04-03 12:02:41 -0700

    Added JS alerts

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -18,10 +18,16 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new(params[:recipe])
 
     if @recipe.save
+      flash[:notice] = "Your recipe was added to Recipe"
       redirect_to("/recipes")
     else
+      @messages = []
+      @recipe.errors.full_messages.each do |message|
+        @messages << message
+      end
+      messages = @messages.join(", ")
+      flash[:alert] = "Errors: #{messages}"
       render("recipes/new.html.erb")
     end
-
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,4 @@
 class Recipe < ActiveRecord::Base
   validates :name, :presence => true
+  validates :ingredients, :presence => true
 end
